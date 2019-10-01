2ac2819948863ea946fe1e79b1281e5b89b83030
Anton <anton.ulasenko@gmail.com>
2019-10-01 16:56:40 +0300

    relation database hw

--- relational_database/config.py
+++ relational_database/config.py
@@ -14,6 +14,6 @@ TEST_DATABASE = {
     "port": 5432
 }
 
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-FIXTURES_PATH = os.path.join(PROJECT_PATH, "fixtures")
+#PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+dir = os.path.dirname(__file__)
+FIXTURES_PATH = os.path.join(dir, os.sep, "fixtures")
