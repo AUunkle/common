df7f230649302fe92b2aa3d75a615defbaec6d3b
Anton <anton.ulasenko@gmail.com>
2019-10-09 02:51:02 +0300

    fix docstring

--- git/homework.py
+++ git/homework.py
@@ -28,7 +28,7 @@ def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
     """
-    If @first and @second has same type should return True
+    If @first and @second has same object should return True
     In another case should return False
     """
     return first is second
